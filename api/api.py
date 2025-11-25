d256afc5594a047dd5fba1845cc003ef9a3a4995
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2025-11-25 02:18:26 +0000

    feat: add an authentication endpoint to the API

--- api/api.py
+++ api/api.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter
-from api.endpoints import orders
+from api.endpoints import orders, auth
 
 api_router = APIRouter()
 
 api_router.include_router(router=orders.router,
                           prefix='/orders', tags=['Pedidos'])
+
+api_router.include_router(
+    router=auth.router, prefix='/auth', tags=['Autenticação'])
