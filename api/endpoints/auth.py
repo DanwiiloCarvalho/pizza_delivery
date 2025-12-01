c2d3a0568ca7721d6c131511dc30ced34e65e9a0
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2025-12-01 13:19:04 +0000

    feat: add /refresh route for generating new access tokens

--- api/endpoints/auth.py
+++ api/endpoints/auth.py
@@ -1,12 +1,12 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, Cookie, status
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
-from core.deps import get_session
+from core.deps import get_session, get_current_user
 from core.security import generate_hash
 from schemas.user_schema import RegisterUserSchema, BaseUserSchema
 from schemas.login_schema import LoginSchema
-from schemas.token_schema import TokenSchema
+from schemas.token_schema import TokenSchema, AccessTokenSchema
 from models.user import User
 from core.authentication import create_token, authenticate_user
 from core.settings import settings as stt
@@ -53,8 +53,8 @@ async def create_account(new_user: RegisterUserSchema, db: AsyncSession = Depend
     status_code=status.HTTP_200_OK,
     response_model=TokenSchema,
     summary='Faz o login do usuário',
-    description='Realiza a autenticação do usuário através de e-mail e senha, devolvendo um token de acesso logo em seguida.',
-    response_description='Retorna o token de acesso e o tipo do token.'
+    description='Realiza a autenticação do usuário através de e-mail e senha, devolvendo um access token e um refresh token logo em seguida.',
+    response_description='Retorna um access token, um refresh token e o tipo dos tokens.'
 )
 async def login(login: Annotated[OAuth2PasswordRequestForm, Depends()], db: AsyncSession = Depends(get_session)):
     user_found = await authenticate_user(
@@ -80,4 +80,36 @@ async def login(login: Annotated[OAuth2PasswordRequestForm, Depends()], db: Asyn
         "access_token": access_token,
         "refresh_token": refresh_token,
         "token_type": "bearer"
+    }
+
+
+@router.post(
+    '/refresh',
+    response_model=AccessTokenSchema,
+    summary='Gera um novo access token',
+    description='Gera um novo access token para utilização das rotas protegidas',
+    response_description='Retorna um access token e o tipo do token'
+)
+async def refresh_access_token(refresh_token: str = Cookie(None), db: AsyncSession = Depends(get_session)):
+    if not refresh_token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail='Refresh token ausente.')
+
+    user_found = await get_current_user(refresh_token, db=db)
+
+    payload_data = {
+        "sub": str(user_found.id),
+        "email": user_found.email,
+        "iat": int(datetime.now(tz=ZoneInfo("America/Sao_Paulo")).timestamp())
+    }
+
+    access_token_expires = timedelta(
+        minutes=int(stt.ACCESS_TOKEN_EXPIRE_MINUTES))
+
+    access_token = create_token(
+        data=payload_data, expires_delta=access_token_expires)
+
+    return {
+        'access_token': access_token,
+        'token_type': 'bearer'
     }
