bafde7c96dcfe34064643b26f058415d4cb8a9b8
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2025-12-01 13:16:25 +0000

    feat: add get_current_user function as a dependency

--- core/deps.py
+++ core/deps.py
@@ -1,8 +1,17 @@
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from typing import Annotated
 from typing import AsyncGenerator
 from core.database import Session
 from core.database import session_context
+from core.authentication import verify_token
+from core.settings import settings as stt
+from fastapi import Depends, status
+from fastapi.security import OAuth2PasswordBearer
 from contextvars import Token
+from fastapi.exceptions import HTTPException
+from schemas.token_schema import TokenDataSchema
+from models.user import User
 
 
 async def get_session() -> AsyncGenerator:
@@ -12,3 +21,29 @@ async def get_session() -> AsyncGenerator:
             yield session
         finally:
             session_context.reset(token)
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl=f"{stt.API_PREFIX}/auth/login")
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], db: AsyncSession = Depends(get_session)) -> User | None:
+
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Não é possível validar as crendenciais",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    payload: dict[str, any] = verify_token(token=token)
+
+    user_id = payload.get('sub')
+
+    if not user_id:
+        raise credentials_exception
+
+    token_data = TokenDataSchema(user_id=user_id)
+
+    query = select(User).filter(User.id == token_data.user_id)
+    user = (await db.execute(query)).unique().scalar_one_or_none()
+    if not user:
+        raise credentials_exception
+    return user
