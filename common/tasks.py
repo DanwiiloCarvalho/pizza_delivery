4c3d6aa1299207180c36146a17063a2d50a532e8
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2025-12-30 18:14:35 +0000

    feat: add email sending functionality with welcome message and HTML template

--- common/tasks.py
+++ common/tasks.py
@@ -1,6 +1,34 @@
 from common.celery_app import celery_app
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from dotenv import load_dotenv
+from pathlib import Path
+import os
+import smtplib
+
+load_dotenv()
+email_user: str = os.getenv(key='EMAIL_USER')
+email_password: str = os.getenv(key='EMAIL_PASSWORD')
+smtp_host: str = os.getenv(key='SMTP_HOST')
+smtp_port: str = os.getenv(key='SMTP_PORT')
 
 
 @celery_app.task
-def send_email(email: str):
-    return f'Email enviado para o endereço {email}'
+def send_email(username: str, email: str):
+    email_msg = MIMEMultipart('alternative')
+
+    email_msg["From"] = email_user
+    email_msg["To"] = email
+    email_msg["Subject"] = "Teste de email com Python"
+    welcome_html = Path('common/welcome.html').read_text(encoding='utf-8')
+    welcome_html = welcome_html.replace('{username}', username)
+
+    email_msg.attach(MIMEText(welcome_html, 'html', 'utf-8'))
+
+    with smtplib.SMTP(host=smtp_host, port=smtp_port) as server:
+        server.starttls()
+        server.login(user=email_user,
+                     password=email_password)
+        server.send_message(email_msg)
+
+    return f'E-mail de boas-vindas enviado com sucesso para {email}'
