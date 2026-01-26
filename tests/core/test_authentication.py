18ad076839b7c843f3f9c284757b9e2d3e6dfe69
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2026-01-26 18:32:43 +0000

    test: refactor token creation tests to include expiration delta parameter

--- tests/core/test_authentication.py
+++ tests/core/test_authentication.py
@@ -9,23 +9,24 @@ import jwt
 
 
 @pytest.mark.parametrize(
-    'sub, email',
+    'sub, email, expires_delta',
     [
-        ('2', 'joao@genericemail.com'),
-        ('1', 'felipe@teste.com'),
-        ('321', 'maria@email.com')
+        ('2', 'joao@genericemail.com', 5),
+        ('32', 'felipe@teste.com', None)
     ]
 )
 @pytest.mark.unit_create_token
-def test_create_token_success(sub, email):
+def test_create_token_success(sub, email, expires_delta):
     payload_data = {
         "sub": sub,
         "email": email,
         "iat": int(datetime.now(tz=ZoneInfo("America/Sao_Paulo")).timestamp())
     }
-
-    token_expires = timedelta(minutes=5)
-    token = create_token(data=payload_data, expires_delta=token_expires)
+    if expires_delta:
+        token_expires = timedelta(minutes=expires_delta)
+        token = create_token(data=payload_data, expires_delta=token_expires)
+    else:
+        token = create_token(data=payload_data)
 
     assert isinstance(token, str) is True
     decoded = jwt.decode(token, stt.SECRET_KEY, stt.ALGORITHM)
